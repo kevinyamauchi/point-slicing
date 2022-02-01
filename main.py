74ae66e59487ee193e5244c10942b31fadb1f558
paddyroddy <patrickjamesroddy@gmail.com>
2022-02-01 16:16:36 +0000

    Use `numpy` rather than `dask` and use `persistent` array

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
-import dask.array as da
 import zarr
+from numpy.random import default_rng
+from zarr.errors import PathNotFoundError
 
 from utils.cli import read_args
 from utils.timer import timer
+from utils.vars import DTYPE, RANDOM_SEED
 
 _file_location = Path(__file__).resolve()
 
@@ -14,22 +16,39 @@ def create_zarr_random_points(
     ndim: int, points_per_dim: int, chunk_size: int
 ) -> zarr.Array:
     """
-    creates a d-dimensional zarr dataset of radnomly distributed points
+    creates a d-dimensional zarr dataset of randomly distributed points
     """
     filepath = (
         _file_location.parent
         / "data"
         / f"random_points_dim{ndim}_points{points_per_dim}.zarr"
     )
-    if filepath.exists():
-        box = zarr.load(filepath)
-    else:
-        box = da.random.random(
-            size=(points_per_dim,) * ndim, chunks=(chunk_size,) * ndim
-        ).to_zarr(filepath)
+    box_shape = (points_per_dim,) * ndim
+    try:
+        box = zarr.open(filepath, mode="r")
+    except PathNotFoundError:
+        box = zarr.open(
+            filepath,
+            mode="w",
+            shape=box_shape,
+            chunks=(chunk_size,) * ndim,
+            dtype=DTYPE,
+        )
+        rng = default_rng(RANDOM_SEED)
+        box[:] = rng.random(box_shape)
     return box
+
+
+def find_2d_slice_of_3d_box(box: zarr.Array, x: int, y: int, z: int) -> zarr.Array:
+    """
+    Finds a 2d slice of a 3d points using linear search by calculating
+    distance to the slicing plane for each point. Points within 0.5 units
+    of the slice are classed as belonging to the slice.
+    """
+    return box[x, y, z]
 
 
 if __name__ == "__main__":
     args = read_args()
-    z = create_zarr_random_points(args.ndim, args.points, args.chunksize)
+    box = create_zarr_random_points(args.ndim, args.points, args.chunksize)
+    find_2d_slice_of_3d_box(box, args.x, args.y, args.z)
--- utils/cli.py
+++ utils/cli.py
@@ -5,6 +5,9 @@ from utils.vars import (
     DEFAULT_CHUNK_SIZE,
     DEFAULT_NDIM,
     DEFAULT_POINTS_PER_DIM,
+    DEFAULT_X,
+    DEFAULT_Y,
+    DEFAULT_Z,
     FLOAT64_BYTES,
 )
 
@@ -19,27 +22,45 @@ def read_args() -> Namespace:
         "-c",
         type=int,
         default=DEFAULT_CHUNK_SIZE,
-        help=f"zarr chunk size: {DEFAULT_CHUNK_SIZE}",
+        help=f"zarr chunk size, default: {DEFAULT_CHUNK_SIZE}",
     )
     parser.add_argument(
         "--ndim",
         "-d",
         type=int,
         default=DEFAULT_NDIM,
-        help=f"box dimensions: {DEFAULT_NDIM}",
+        help=f"box dimensions, default: {DEFAULT_NDIM}",
     )
     parser.add_argument(
         "--points",
         "-p",
         type=int,
         default=DEFAULT_POINTS_PER_DIM,
-        help=f"points per dimension: {DEFAULT_POINTS_PER_DIM}",
+        help=f"points per dimension, default: {DEFAULT_POINTS_PER_DIM}",
+    )
+    parser.add_argument(
+        "-x",
+        type=int,
+        default=DEFAULT_X,
+        help=f"x-coordinate of slice, default: {DEFAULT_X}",
+    )
+    parser.add_argument(
+        "-y",
+        type=int,
+        default=DEFAULT_Y,
+        help=f"y-coordinate of slice, default: {DEFAULT_Y}",
+    )
+    parser.add_argument(
+        "-z",
+        type=int,
+        default=DEFAULT_Z,
+        help=f"z-coordinate of slice, default: {DEFAULT_Z}",
     )
     args = parser.parse_args()
     logger.info(
         f"dimensions: {args.ndim}, "
         f"points per dimensions: {args.points}, "
         f"chunk size: {args.chunksize}, "
-        f"requested number of bytes {FLOAT64_BYTES*args.points**args.ndim:e}"
+        f"number of bytes {FLOAT64_BYTES*args.points**args.ndim:e}"
     )
     return args
