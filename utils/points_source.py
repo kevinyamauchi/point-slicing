10a99e38087263c4bffef16cca8f5bd207e69f52
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-02-02 15:29:53 +0100

    initial point source

---
+++ utils/points_source.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+import numpy as np
+
+
+class PointsSource(ABC):
+    def __init__(self, data):
+        self._load_data(data)
+
+    @abstractmethod
+    def _load_data(self, data):
+        raise NotImplementedError
+
+    @abstractmethod
+    def slice_data(self, slice_indices):
+        raise NotImplementedError
+
+    @abstractmethod
+    def ray_intersection(
+        self, ray_start: np.ndarray, ray_direction: np.ndarray
+    ) -> List[int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def nearest_neighbor(self, coordinate: np.ndarray) -> int:
+        raise NotImplementedError
