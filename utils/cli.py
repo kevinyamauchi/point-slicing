19018a8a6954bc59e909df78c8797e29810c1723
paddyroddy <patrickjamesroddy@gmail.com>
2022-02-02 10:52:00 +0000

    Switch to Euler angles

--- utils/cli.py
+++ utils/cli.py
@@ -4,12 +4,12 @@ import numpy as np
 
 from utils.logger import logger
 from utils.vars import (
+    DEFAULT_ALPHA,
+    DEFAULT_BETA,
     DEFAULT_CHUNK_SIZE,
+    DEFAULT_GAMMA,
     DEFAULT_NDIM,
     DEFAULT_POINTS_PER_DIM,
-    DEFAULT_X,
-    DEFAULT_Y,
-    DEFAULT_Z,
     DTYPE,
 )
 
@@ -34,29 +34,29 @@ def read_args() -> Namespace:
         help=f"box dimensions, default: {DEFAULT_NDIM}",
     )
     parser.add_argument(
+        "-a",
+        type=int,
+        default=DEFAULT_ALPHA,
+        help=f"Euler angle alpha in degrees in range [0,360), default: {DEFAULT_ALPHA}",
+    )
+    parser.add_argument(
+        "-b",
+        type=int,
+        default=DEFAULT_BETA,
+        help=f"Euler angle beta in degrees in range [0,180], default: {DEFAULT_BETA}",
+    )
+    parser.add_argument(
+        "-g",
+        type=int,
+        default=DEFAULT_GAMMA,
+        help=f"Euler angle gamma in degrees in range [0,360), default: {DEFAULT_GAMMA}",
+    )
+    parser.add_argument(
         "--points",
         "-p",
         type=int,
         default=DEFAULT_POINTS_PER_DIM,
         help=f"points per dimension, default: {DEFAULT_POINTS_PER_DIM}",
-    )
-    parser.add_argument(
-        "-x",
-        type=int,
-        default=DEFAULT_X,
-        help=f"Euler angle of x-coordinate, default: {DEFAULT_X}",
-    )
-    parser.add_argument(
-        "-y",
-        type=int,
-        default=DEFAULT_Y,
-        help=f"Euler angle of y-coordinate default: {DEFAULT_Y}",
-    )
-    parser.add_argument(
-        "-z",
-        type=int,
-        default=DEFAULT_Z,
-        help=f"Euler angle of z-coordinate, default: {DEFAULT_Z}",
     )
     args = parser.parse_args()
     logger.info(
--- utils/vars.py
+++ utils/vars.py
@@ -1,10 +1,10 @@
 import numpy as np
 
+DEFAULT_ALPHA: int = 0
+DEFAULT_BETA: int = 0
 DEFAULT_CHUNK_SIZE: int = 100
+DEFAULT_GAMMA: int = 0
 DEFAULT_NDIM: int = 3
 DEFAULT_POINTS_PER_DIM: int = 1_000
-DEFAULT_X: int = 0
-DEFAULT_Y: int = 0
-DEFAULT_Z: int = 0
 DTYPE: type = np.float32
 RANDOM_SEED: int = 30
